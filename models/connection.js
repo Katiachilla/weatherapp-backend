18ae8c2f18db74e17897f454ff834773b6f34a3b
katia <ghilasmaouche89@gmail.com>
2023-06-26 00:29:22 +0200

    configure dotenv for conn string and apikey

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://katiachilla:d1SCeKuT8o8ajGcE@cluster0.ycy5frs.mongodb.net/weatherapp';
+const connectionString = process.env.CONNECTION_STRING;
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
